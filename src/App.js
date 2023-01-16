304db2ad43a682410adc5f2a730e9c4da7ce7618
tingamapuro04 <alponcemobutu100@gmail.com>
2023-01-16 17:23:00 +0300

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { getData } from './features/dataSlice';
 import './App.css';
+import Posts from './components/Posts';
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(()=> {
+    dispatch(getData())
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Posts />
     </div>
   );
 }
